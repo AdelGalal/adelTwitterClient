6820623b2a7d1b12065c7838a4343224cb5ffb61
adelhegazy <adel.fci.egy@gmail.com>
2017-07-11 15:20:39 +0200

    Update the gradle version

--- app/src/main/java/adel/twitterclient/screens/dataController/FollowerDataController.java
+++ app/src/main/java/adel/twitterclient/screens/dataController/FollowerDataController.java
@@ -1,16 +1,9 @@
 package adel.twitterclient.screens.dataController;
 
 import android.content.Context;
-
 import com.google.gson.JsonElement;
-import com.j256.ormlite.dao.Dao;
-
-import java.util.concurrent.Callable;
-
-import adel.twitterclient.businessModel.DTO.FollowerInfo;
 import adel.twitterclient.businessModel.DTO.FollowerResponse;
 import adel.twitterclient.businessModel.gson.Gson;
-import adel.twitterclient.database.DatabaseHelper;
 import adel.twitterclient.twitter.TwitterClientHelper;
 import adel.twitterclient.screens.viewController.FollowersActivity;
 import retrofit2.Response;
@@ -21,16 +14,11 @@ import retrofit2.Response;
 
 public class FollowerDataController {
     private Context context;
-    private String cursor;
     private FollowerResponse followerResponse;
     private FollowersListener followersListener;
-    private DatabaseHelper databaseConfig;
     public FollowerDataController(Context context,String cursor)
     {
         this.context=context;
-        this.cursor=cursor;
-        databaseConfig = new DatabaseHelper(context);
-
     }
 
     public void setFollowersListener(FollowersListener followersListener) {
@@ -38,44 +26,17 @@ public class FollowerDataController {
     }
     public void startConnectionToGetFollowers(String cursor) {
         final Response<JsonElement> response = TwitterClientHelper.GetFollowers(TwitterClientHelper.GetCurrentUserId(), FollowersActivity.NUM_OF_ITEMS, cursor);
-        if (response.isSuccessful()) {
-            followerResponse = Gson.parseFollowers(response.body());
-            if (response.isSuccessful())
-            {
-                followersListener.notifyFollowersData(followerResponse);
-            }
-            else
-            {
-                followersListener.notifyConnectionOrDataError();
-            }
+        if (response!=null) {
+            if (response.isSuccessful()) {
+                followerResponse = Gson.parseFollowers(response.body());
+                if (response.isSuccessful()) {
+                    followersListener.notifyFollowersData(followerResponse);
+                } else {
+                    followersListener.notifyConnectionOrDataError();
+                }
 
+            }
         }
     }
 
-    public void accessAndUpdateDatabase(final FollowerResponse followerResponse) {
-
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    final Dao<FollowerInfo, Integer> followersDao = databaseConfig.getFollowerInfoDao();
-                    followersDao.callBatchTasks(new Callable<Void>() {
-                        @Override
-                        public Void call() throws Exception {
-                            if (followerResponse != null && followerResponse.getFollowerOfUsers() != null) {
-                                for (FollowerInfo f : followerResponse.getFollowerOfUsers())
-                                    followersDao.createOrUpdate(f);
-                            }
-                            return null;
-                        }
-                    });
-
-
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        }).start();
-
-    }
 }
